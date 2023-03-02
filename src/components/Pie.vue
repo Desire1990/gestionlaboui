8c89583359a02b234f43d6d412fbc691d4e2e376
Desire1990 <desnduwim26@gmail.com>
2023-03-02 15:54:45 +0200

    stat page

---
+++ src/components/Pie.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="center-chart">
+    <apexchart width="600" type="pie" :options="chartOptions" :series="series">
+    </apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Apex charts",
+  data: () => ({
+    chartOptions: {
+      chart: {
+        id: "apexchart-example",
+      },
+      title: {
+        text: "Commande distribution",
+        align: "center",
+        margin: 40,
+        style: {
+          fontSize: "20px",
+          fontWeight: "bold",
+          color: "#263238",
+        },
+      },
+      colors: ['#2E93fA', '#46DA62', '#242E8A', '#E91E63', '#FF9800'],
+      dataLabels: {
+         enabled: true,
+      },
+      legend: {
+        show: true,
+        position: 'bottom',
+      },
+        labels: ["Apple", "Mango", "Orange", "Watermelon"],
+    },
+    series: [44, 55, 41, 17],
+  }),
+};
+</script>
+<style scoped>
+.center-chart {
+display: flex;
+align-items: center;
+justify-content: center;
+}
+</style>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "Apex charts",
+  data: () => ({
+    chartOptions: {
+      chart: {
+        id: "apexchart-example",
+      },
+      title: {
+        text: "Fruit distribution",
+        align: "center",
+        margin: 40,
+        style: {
+          fontSize: "20px",
+          fontWeight: "bold",
+          color: "#263238",
+        },
+      },
+      colors: ['#2E93fA', '#46DA62', '#242E8A', '#E91E63', '#FF9800'],
+      dataLabels: {
+         enabled: true,
+      },
+      legend: {
+        show: true,
+        position: 'bottom',
+      },
+        labels: ["Apple", "Mango", "Orange", "Watermelon"],
+    },
+    series: [44, 55, 41, 17],
+  }),
+};
